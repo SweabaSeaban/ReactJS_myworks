bca4f124c45987a7b535a3a64f6ff77f21ac9e27
SweabaSeaban <sweabaseaban@gmail.com>
2024-01-20 12:04:58 +0530

    commit

---
+++ custom_hooks/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
+    style={{backgroundImage: `url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`}}
+    > 
+      App
+    </div>
+  )
+}
+
+export default App
